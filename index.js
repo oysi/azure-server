d3dad8d4e850dfcf608e8f352ac2298a09d1334c
Øystein Veisetaune Svingen <oystein.svingen@omega365.com>
2023-03-16 13:34:49 +0100

    changes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ console.log("Hello, World!");
 
 console.log("done");
 
+const mssql = require("mssql");
+
 const express = require("express");
 const app = express();
 // const port = 3000;
@@ -23,3 +25,23 @@ app.listen(443, () => {
 app.listen(8080, () => {
 	console.log("Listening on port 8080...");
 });
+
+
+
+console.log(process.env.ENV_VAR);
+
+const config = {
+    user: 'username', // better stored in an app setting such as process.env.DB_USER
+    password: 'password', // better stored in an app setting such as process.env.DB_PASSWORD
+    server: 'your_server.database.windows.net', // better stored in an app setting such as process.env.DB_SERVER
+    port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
+    database: 'AdventureWorksLT', // better stored in an app setting such as process.env.DB_NAME
+    authentication: {
+        type: 'default'
+    },
+    options: {
+        encrypt: true
+    }
+}
+
+
